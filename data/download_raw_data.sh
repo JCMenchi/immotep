3d2681df906654b4eb57c9646be0bd300e238cd0
JC Menchi <jean.menchi@gmail.com>
2021-08-12 07:57:00 +0200

    Add script to get data from internet

---
+++ data/download_raw_data.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+
+# Get info about region, department, city
+
+# Official zipcode
+# readable URL is
+# https://datanova.legroupe.laposte.fr/explore/dataset/laposte_hexasmal/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true
+
+if [ ! -f zip_code.csv ]; then
+    curl 'https://datanova.legroupe.laposte.fr/explore/dataset/laposte_hexasmal/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true' > zip_code.csv
+fi
+
+#
+# Use https://geo.api.gouv for basic administrative info
+#
+# Use laposte for geojson info
+#  https://datanova.legroupe.laposte.fr/api/records/1.0/geopreview/?disjunctive.reg_name=true&sort=year&rows=1000&clusterprecision=6&dataset=georef-france-region&timezone=Europe%2FBerlin
+# 
+if [ ! -f regions.geojson ]; then
+    curl 'https://datanova.legroupe.laposte.fr/explore/dataset/georef-france-region/download/?format=geojson&timezone=Europe/Berlin' > regions.geojson
+fi
+
+if [ ! -f regions.json ]; then
+    curl 'https://geo.api.gouv.fr/regions' > regions.json
+fi
+
+if [ ! -f departements.json ]; then
+    curl 'https://geo.api.gouv.fr/departements' > departements.json
+fi
+
+if [ ! -f departements.geojson ]; then
+    curl 'https://datanova.legroupe.laposte.fr/explore/dataset/georef-france-departement/download/?format=geojson&timezone=Europe/Berlin' > departements.geojson
+fi
+
+if [ ! -f communes.json ]; then
+    curl 'https://geo.api.gouv.fr/communes' > communes.json
+fi
+
+#
+# Get sales infos from DVF database
+# https://www.data.gouv.fr/fr/datasets/demandes-de-valeurs-foncieres/
+#
+
+DVF_BASE_URL=https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20210330-
+
+if [ ! -f valeursfoncieres-2020.txt ]; then
+    curl ${DVF_BASE_URL}102242/valeursfoncieres-2020.txt > valeursfoncieres-2020.txt
+fi
+
+if [ ! -f valeursfoncieres-2019.txt ]; then
+    curl ${DVF_BASE_URL}102025/valeursfoncieres-2019.txt > valeursfoncieres-2019.txt
+fi
+
+if [ ! -f valeursfoncieres-2018.txt ]; then
+    curl ${DVF_BASE_URL}101812/valeursfoncieres-2018.txt > valeursfoncieres-2018.txt
+fi
+
+if [ ! -f valeursfoncieres-2017.txt ]; then
+    curl ${DVF_BASE_URL}101548/valeursfoncieres-2017.txt > valeursfoncieres-2017.txt
+fi
+
+if [ ! -f valeursfoncieres-2016.txt ]; then
+    curl ${DVF_BASE_URL}101325/valeursfoncieres-2016.txt > valeursfoncieres-2016.txt
+fi
+
+
+# Build database from raw data
+if [ ! -f immbzh.db ]; then
+    ../srv/immotep -f immbzh.db loadconf --region regions.geojson --department departements.geojson --city communes.json
+    ../srv/immotep -f immbzh.db load -z zip_code.csv valeursfoncieres-20*.txt
+    ../srv/immotep -f immbzh.db geocode
+fi
