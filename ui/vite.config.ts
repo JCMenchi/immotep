608b49c8252d96c37e5d66fb30ea35d4578a6d2e
JC Menchi <jean.menchi@gmail.com>
2025-10-19 10:50:03 +0200

    Set up UI debug and proxy for vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 import oxlintPlugin from 'vite-plugin-oxlint'
 
 export default defineConfig({
+  base: '/immotep/',
   plugins: [react(), oxlintPlugin()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
